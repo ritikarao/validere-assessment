55bd998444b5bc4353b94992b9d566442ce6bfad
Ritika Rao <ritika312@hotmail.com>
2021-08-19 20:31:12 -0400

    Sourcing the data locally for subsequent runs of the script with the same parameters

--- crude_monitor.py
+++ crude_monitor.py
@@ -5,8 +5,6 @@ import pandas as pd
 import os
 import argparse
 
-URL = "https://www.crudemonitor.ca/savePHPExcel.php"
-
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--crude_acronym", required = True, help = "crude oil acronym")
 ap.add_argument("-n", "--name", required = True, help = "crude oil name")
@@ -28,50 +26,67 @@ limit = float(args.limit)
 name.title()
 acronym.upper()
 
-database = "crudes"
 
-form_data = {
-    "date1noscript": "",
-    "date2noscript": "",
-    "trendProperty": "AbsoluteDensity",
-    "acr": acronym,
-    "name": name,
-    "db": database,
-    "basicanalysis[]": "AbsoluteDensity",
-    "options": "on",
-    "date1": start_date,
-    "date2": end_date,
-    "format": "Export.CSV",
-    "daterangepicker_start": "",
-    "daterangepicker_end": "",
-}
+def request_data(acronym, name, start_date, end_date):
 
-data = requests.post(url=URL, data=form_data)
+    URL = "https://www.crudemonitor.ca/savePHPExcel.php"
 
-file = open("Density.csv", "wb")
-file.write(data.content)
-file.close()
+    database = "crudes"
 
-df = pd.read_csv('Density.csv', usecols = ['Date', 'Density (kg/m^3)'])
-df.rename(columns = {'Density (kg/m^3)':'Density'}, inplace = True)
+    form_data = {
+        "date1noscript": "",
+        "date2noscript": "",
+        "trendProperty": "AbsoluteDensity",
+        "acr": acronym,
+        "name": name,
+        "db": database,
+        "basicanalysis[]": "AbsoluteDensity",
+        "options": "on",
+        "date1": start_date,
+        "date2": end_date,
+        "format": "Export.CSV",
+        "daterangepicker_start": "",
+        "daterangepicker_end": "",
+    }
+
+    data = requests.post(url=URL, data=form_data)
+    return data
+
+try:
+    df = pd.read_csv(f"saved_queries/{acronym}_{name}_{start_date}_{end_date}_{operation}_{limit}.csv", index_col=0)
+    if df.empty:
+        print("No results for the dates and condition specified!")
+    else:
+        print(df)
+
+except:
+    data = request_data(acronym, name, start_date, end_date)
+
+    file = open("Density.csv", "wb")
+    file.write(data.content)
+    file.close()
+
+    df = pd.read_csv('Density.csv', usecols = ['Date', 'Density (kg/m^3)'])
+    df.rename(columns = {'Density (kg/m^3)':'Density'}, inplace = True)
 
 
-if operation == 'greater_than' or operation == '>':
-    df.drop(df[df.Density <= limit].index, inplace = True)
-elif operation == 'lesser_than' or operation == '<':
-    df.drop(df[df.Density >= limit].index, inplace = True)
-elif operation == 'greater_than_equal_to' or operation == '>=':
-    df.drop(df[df.Density < limit].index, inplace = True)
-elif operation == 'lesser_than_equal_to' or operation == '<=':
-    df.drop(df[df.Density > limit].index, inplace = True)
-elif operation == 'equal_to' or operation == '=' or operation == '==':
-    df.drop(df[df.Density != limit].index, inplace = True)
+    if operation == 'greater_than' or operation == '>':
+        df.drop(df[df.Density <= limit].index, inplace = True)
+    elif operation == 'lesser_than' or operation == '<':
+        df.drop(df[df.Density >= limit].index, inplace = True)
+    elif operation == 'greater_than_equal_to' or operation == '>=':
+        df.drop(df[df.Density < limit].index, inplace = True)
+    elif operation == 'lesser_than_equal_to' or operation == '<=':
+        df.drop(df[df.Density > limit].index, inplace = True)
+    elif operation == 'equal_to' or operation == '=' or operation == '==':
+        df.drop(df[df.Density != limit].index, inplace = True)
 
-df = df.reset_index(drop = True)
+    df = df.reset_index(drop = True)
 
-if df.empty:
-    print("No results for the dates and condition specified!")
-else:
-    print(df)
+    if df.empty:
+        print("No results for the dates and condition specified!")
+    else:
+        print(df)
 
-os.remove('Density.csv')
+    os.remove('Density.csv')
+    df.to_csv(f"saved_queries/{acronym}_{name}_{start_date}_{end_date}_{operation}_{limit}.csv")
